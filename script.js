98d31b5feeb028e2cc5eca906352003c59f03379
Ganesh Khamat <ganeshkhamat20@gmail.com>
2022-01-01 11:19:33 +0530

    console added

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function compute()
 
     var dateNow = new Date();
     var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
-    
+    console.log("this is edited by sagar yadav")
     var resultDisplay = document.getElementById("result");
     resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
